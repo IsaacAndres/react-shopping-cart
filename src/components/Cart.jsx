ab3b6f066c68e7cc395d4909993fedbdc2f1a2c8
Isaac <isaac.andres.figueroa@gmail.com>
2023-07-28 11:48:04 -0400

    first commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,38 @@
+import { useId } from 'react'
+import { CartIcon, ClearCartIcon } from './Icons'
+import './Cart.css'
+import { useCart } from '../hooks/useCart'
+import { CartItem } from './CartItem'
+
+export const Cart = () => {
+  const cartCheckboxId = useId()
+  const { cart, clearCart, addToCart } = useCart()
+
+  return (
+    <>
+      <label className='cart-button' htmlFor={cartCheckboxId}>
+        <CartIcon />
+      </label>
+      <input type="checkbox"  id={cartCheckboxId} hidden />
+
+      <aside className="cart">
+        <ul>
+          {
+            cart.map(product => (
+              <CartItem 
+                key={product.id} 
+                addToCart={() => addToCart(product)}
+                {...product}
+              />
+            ))
+          }
+        </ul>
+
+        <button onClick={clearCart}>
+          <ClearCartIcon />
+        </button>
+      </aside>
+    </>
+  )
+}
+
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,16 @@
+export function CartItem ({ thumbnail, title, price, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title} />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+      <footer>
+        <small>
+          Cant: {quantity}
+        </small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
